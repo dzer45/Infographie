22befee57099d1e35cef6ba91eb7333aa86a101d
Arif <dzer45@gmail.com>
2015-01-26 10:40:04 +0100

    Fil de fer : ok

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
 #include "geometry.h"
-#include <cmath>
 #include "model.h"
 
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
 const int width = 800;
 const int height = 800;
-const int depth = 255;
- 
-Model *model = NULL;
 
-
-void line(TGAImage &image, int x0, int y0, int x1, int y1) {
+void line(TGAImage &image, int x0, int y0, int x1, int y1, TGAColor color) {
 	bool steep = false;
 	if (std::abs(x0-x1)<std::abs(y0-y1)) {
 		std::swap(x0, y0);
@@ -32,16 +32,16 @@ void line(TGAImage &image, int x0, int y0, int x1, int y1) {
 			y = y0;
 		}
 		if (steep) {
-			image.set(y, x, TGAColor(255, 1));
+			image.set(y, x, color);
 		} else {
-			image.set(x, y, TGAColor(255, 1));
+			image.set(x, y, color);
 		}
 	}
 
 
 }
 
-void triangle(TGAImage &image, int x0, int y0, int x1, int y1, int x2, int y2){
+void triangle(TGAImage &image, int x0, int y0, int x1, int y1, int x2, int y2, TGAColor color){
 	if (x2<x0) {
   		std::swap(y0,y2);
   		std::swap(x0,x2);
@@ -57,9 +57,9 @@ void triangle(TGAImage &image, int x0, int y0, int x1, int y1, int x2, int y2){
 			float t2 = (x-x0)/(float)(x2-x0);
 			int y = y0*(1.-t)+y1*t;
 			int y3 = y0*(1.-t2)+y2*t2;
-			line(image, x, y, x, y3);
+			line(image, x, y, x, y3, color);
 		}else	
-			line(image, x0, y0, x1, y1);
+			line(image, x0, y0, x1, y1, color);
 			
 	}
 	for( int x=x1; x<=x2; x++ ){
@@ -68,33 +68,30 @@ void triangle(TGAImage &image, int x0, int y0, int x1, int y1, int x2, int y2){
 			float t2 = (x-x1)/(float)(x2-x0);
 			int y = y1*(1.-t)+y2*t;
 			int y3 = y0*(1.-t2)+y2*t2;
-			line(image, x, y, x, y3);
+			line(image, x, y, x, y3, color);
 		} else
-			line(image, x1, y1, x2, y2);
+			line(image, x1, y1, x2, y2, color);
 	}
 
 }
 
 int main() {
 	model = new Model("obj/african_head.obj");
-
-	TGAImage image(width,height, TGAImage::RGB );
-	for( int i=0; i < model->nfaces(); i++) {
-		std::vector<int> face = model -> face(i);
-		Vec3i screen_coords[3];
-		Vec3f world_coords[3];
-		for ( int j=0; j<3; j++) {
-			Vec3f v = model->vert(face[j]);
-			screen_coords[j] = Vec3i((v.x+1.)*width/2., (v.y+1.)*height/2., (v.z+1.)*depth/2);
-		//	world_coords[j] = v ;
-		}
-//		line
-
-	}
-	
-
-
-
+	TGAImage image(width, height, TGAImage::RGB);
+ 	for (int i=0; i<model->nfaces(); i++) {
+ 		std::vector<int> face = model->face(i);
+		for (int j=0; j<3; j++) {
+ 			Vec3f v0 = model->vert(face[j]);
+ 			Vec3f v1 = model->vert(face[(j+1)%3]);
+ 			int x0 = (v0.x+1.)*width/2.;
+ 			int y0 = (v0.y+1.)*height/2.;
+ 			int x1 = (v1.x+1.)*width/2.;
+ 			int y1 = (v1.y+1.)*height/2.;
+ 			line(image,x0, y0, x1, y1, white);
+ 		}
+	}	
 	image.flip_vertically();
 	image.write_tga_file("dump.tga");
+	delete model;
+	return 0;
 }

3f83dd8a1148344979f18167db84473d29f40799
Arif <dzer45@gmail.com>
2015-02-19 22:08:19 +0100

    TP2 : Dessin avec triangle rempli

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstdlib>
 #include <cmath>
 #include "tgaimage.h"
 #include "model.h"
@@ -35,6 +36,27 @@ void line(TGAImage &image,TGAColor color , int x0, int y0, int x1, int y1) {
     }
 }
 
+void triangle(TGAImage &image, TGAColor color,Vec2i t0, Vec2i t1, Vec2i t2){	
+	if (t0.y==t1.y && t0.y==t2.y) return;
+	if (t0.y> t1.y) std::swap(t0,t1);
+	if (t0.y> t2.y) std::swap(t0,t2);
+	if (t1.y> t2.y) std::swap(t1,t2);
+	
+	int total_height = t2.y-t0.y;
+	for( int i = 0; i < total_height; i++){
+		bool second_half = i > t1.y-t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+		float alpha = (float)i/total_height;
+		float beta = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height;
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = second_half ? t1+(t2-t1)*beta : t0 + (t1-t0)*beta;
+		if(A.x > B.x) std::swap(A,B);
+		for(int j = A.x ; j <= B.x ; j++){
+			image.set(j,t0.y+i,color);
+		}
+	}
+}
+
 int main(int argc, char** argv) {	
 	Model *model = NULL;
 	if(2==argc){
@@ -42,21 +64,17 @@ int main(int argc, char** argv) {
 	}else {
 		model = new Model("obj/african_head.obj");
 	}
-	TGAColor red = TGAColor(255,0,0,255);
 	int width = 800;
 	int height = 800;
 	TGAImage image(width,height,TGAImage::RGB);
 	for( int i=0; i<model->nfaces(); i++){
 		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
 		for(int j=0; j<3 ; j++){
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x+1.)*width/2;
-			int y0 = (v0.y+1.)*height/2;
-			int x1 = (v1.x+1.)*width/2;
-			int y1 = (v1.y+1.)*height/2;
-			line(image,red,x0,y0,x1,y1);
+			Vec3f world_coords = model->vert(face[j]);
+			screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.);
 		}
+		triangle(image,TGAColor(255,0,0,255), screen_coords[0], screen_coords[1], screen_coords[2]);
 	}
 	image.flip_vertically();
 	image.write_tga_file("dump.tga");

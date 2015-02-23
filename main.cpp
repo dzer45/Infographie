ca1644ab0813c354ee638c54d592a2e6f2d8438b
Arif <dzer45@gmail.com>
2015-02-24 00:12:10 +0100

    Refactoring

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <vector>
 #include <iostream>
-
+#include <limits>
+#include <cmath>
+#include <cstdlib>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
-#include "our_gl.h"
+
+Matrix Viewport;
+Matrix Projection;
+Matrix ModelView;
+Vec3f varying_intensity;
 
 Model *model     = NULL;
 const int width  = 800;
@@ -15,22 +21,79 @@ Vec3f       eye(0,-1,3);
 Vec3f    center(0,0,0);
 Vec3f        up(0,1,0);
 
-struct GouraudShader : public IShader {
-    Vec3f varying_intensity; // written by vertex shader, read by fragment shader
+Matrix  viewport(int x, int y, int w, int h) {
+    Viewport = Matrix::identity();
+    Viewport[0][3] = x+w/2.f;
+    Viewport[1][3] = y+h/2.f;
+    Viewport[2][3] = 255.f/2.f;
+    Viewport[0][0] = w/2.f;
+    Viewport[1][1] = h/2.f;
+    Viewport[2][2] = 255.f/2.f;
+     return Viewport;
+}
 
-    virtual Vec4f vertex(int iface, int nthvert) {
-        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
-        gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;     // transform it to screen coordinates
-        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir); // get diffuse lighting intensity
-        return gl_Vertex;
-    }
+Matrix  projection(float coeff) {
+    Projection = Matrix::identity();
+    Projection[3][2] = coeff;
+    return Projection;
+}
 
-    virtual bool fragment(Vec3f bar, TGAColor &color) {
-        float intensity = varying_intensity*bar;   // interpolate intensity for the current pixel
-        color = TGAColor(255, 255, 255)*intensity; // well duh
-        return false;                              // no, we do not discard this pixel
+Matrix  lookat(Vec3f eye, Vec3f center, Vec3f up) {
+    Vec3f z = (eye-center).normalize();
+    Vec3f x = cross(up,z).normalize();
+    Vec3f y = cross(z,x).normalize();
+    ModelView = Matrix::identity();
+    for (int i=0; i<3; i++) {
+        ModelView[0][i] = x[i];
+        ModelView[1][i] = y[i];
+        ModelView[2][i] = z[i];
+        ModelView[i][3] = -center[i];
     }
-};
+    return ModelView;
+}
+
+Vec3f barycentric(Vec2f A, Vec2f B, Vec2f C, Vec2f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) 
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); 
+}
+
+void triangle(Vec4f *pts, TGAImage &image, TGAImage &zbuffer) {
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::min(bboxmin[j], pts[i][j]/pts[i][3]);
+            bboxmax[j] = std::max(bboxmax[j], pts[i][j]/pts[i][3]);
+        }
+    }
+    Vec2i P;
+    TGAColor color;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f c = barycentric(proj<2>(pts[0]/pts[0][3]), proj<2>(pts[1]/pts[1][3]), proj<2>(pts[2]/pts[2][3]), proj<2>(P));
+            float z = pts[0][2]*c.x + pts[1][2]*c.y + pts[2][2]*c.z;
+            float w = pts[0][3]*c.x + pts[1][3]*c.y + pts[2][3]*c.z;
+            int frag_depth = std::max(0, std::min(255, int(z/w+.5)));
+            if (c.x<0 || c.y<0 || c.z<0 || zbuffer.get(P.x, P.y)[0]>frag_depth) continue;
+	    float intensity = varying_intensity*c;   
+            color = TGAColor(255, 255, 255)*intensity;
+            bool discard = false;
+            if (!discard) {
+                zbuffer.set(P.x, P.y, TGAColor(frag_depth));
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
+}
+
 
 int main(int argc, char** argv) {
     if (2==argc) {
@@ -44,22 +107,21 @@ int main(int argc, char** argv) {
     projection(-1.f/(eye-center).norm());
     light_dir.normalize();
 
-    TGAImage image  (width, height, TGAImage::RGB);
-    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
+    TGAImage image  (width, height, 3);
 
-    GouraudShader shader;
     for (int i=0; i<model->nfaces(); i++) {
         Vec4f screen_coords[3];
         for (int j=0; j<3; j++) {
-            screen_coords[j] = shader.vertex(i, j);
+		Vec4f gl_Vertex = embed<4>(model->vert(i, j));
+        	gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;
+        	varying_intensity[j] = std::max(0.f, model->normal(i, j)*light_dir);
+            	screen_coords[j] = gl_Vertex;
         }
-        triangle(screen_coords, shader, image, zbuffer);
+        triangle(screen_coords, image, zbuffer);
     }
 
     image.  flip_vertically(); // to place the origin in the bottom left corner of the image
-    zbuffer.flip_vertically();
     image.  write_tga_file("output.tga");
-    zbuffer.write_tga_file("zbuffer.tga");
 
     delete model;
     return 0;
